88389b83f62d774344bb23fff098d2384901c4a7
Bernhard <bernhard.robert.pichler@gmail.com>
2014-03-01 23:55:36 +0100

    Fixed RichTextField demo
    It turns out that WebGL can't render if a padding-left style is applied
    to the canvas. It worked fine with Chrome but not in Dartium !?

--- web/richtextfield/index.dart
+++ web/richtextfield/index.dart
@@ -2,30 +2,30 @@ import 'dart:html' show CanvasElement, querySelector, TextAreaElement, ButtonEle
 import 'package:stagexl/stagexl.dart';
 import 'package:stagexl_richtextfield/stagexl_richtextfield.dart';
 
+CanvasElement canvas = querySelector("#stage");
+Stage stage = new Stage(canvas, webGL: true);
+RenderLoop renderLoop = new RenderLoop();
+
 void main() {
 
-  CanvasElement canvas = querySelector("#richtext");
-  TextAreaElement textarea = querySelector('#texttodraw');
+  renderLoop.addStage(stage);
+
   ButtonElement reload = querySelector('#reload');
-  Stage stage = new Stage(canvas, webGL: true)
-    ..scaleMode = StageScaleMode.SHOW_ALL;
-  RenderLoop render = new RenderLoop();
+  TextAreaElement textarea = querySelector('#texttodraw');
   RichTextFormat format = new RichTextFormat('Calibri, sans-serif', 25, 0x000000, align: TextFormatAlign.LEFT);
   RichTextFormat excited = format.clone()..bold=true..italic=true..size=30..color=0xFF00FF;
+
   RichTextField rtf = new RichTextField('',format)
     ..presets['excited'] = excited
     ..text = textarea.value
-    ..width = stage.sourceWidth
-    ..height = stage.sourceHeight
+    ..width = stage.sourceWidth - 20
+    ..height = stage.sourceHeight - 10
     ..wordWrap = true
-    ..y = 0;
-
-  render.addStage(stage);
-  stage.addChild(rtf);
-
+    ..x = 10
+    ..y = 5
+    ..addTo(stage);
 
   reload.onClick.listen((e) {
     rtf.text = textarea.value;
   });
-
 }
